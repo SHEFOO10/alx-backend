b854e60a28dbe017366c3091fd52b62401b43742
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-25 00:09:38 +0000

    fixed

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,7 +6,8 @@ from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+    Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
