7c88ef3937f54e2d9cbe04180ee809b6b6340c3e
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-03 03:20:38 +0300

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,6 +5,7 @@ from flask_babel import Babel
 
 
 class Config:
+    """ Config Class for languages """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
