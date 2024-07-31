22610774d9b0ae04e1e8959bb2250eb3b2694ca6
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-08-01 02:49:10 +0300

    mistyped , with . in line 50

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -50,7 +50,7 @@ const jobs = [
   const queue = kue.createQueue();
 
   jobs.forEach((jobData) => {
-    const job = queue.create('push_notification_code_2'. jobData);
+    const job = queue.create('push_notification_code_2', jobData);
     job.save((err) => {
       if (!err) console.log(`Notification job created: ${job.id}`)
         else console.log('Notification job failed');
