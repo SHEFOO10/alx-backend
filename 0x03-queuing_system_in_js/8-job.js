6f61394f848ea553d5410ddceaec3d919163cbe8
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-08-01 03:39:11 +0300

    call save method in seperate line

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -2,10 +2,12 @@ function createPushNotificationsJobs(jobs, queue) {
     if (!Array.isArray(jobs))
         throw new Error('Jobs is not an array');
     jobs.forEach((jobData) => {
-        const job = queue.create('push_notification_code_3', jobData)
-        .save((err) => {
+        const job = queue.create('push_notification_code_3', jobData);
+        job.save((err) => {
+            console.log(job.id);
             if (!err) console.log(`Notification job created: ${job.id}`);
         });
+        console.log(job)
         job.on('complete', () => console.log(`Notification job ${job.id} completed`));
         job.on('failed', (errorMessage) => console.log(`Notification job ${job.id} failed: ${errorMessage}`));
         job.on('progress', (progress) => console.log(`Notification job ${job.id} ${progress}% complete`));
