e31e842b721473f36388a01ccb794be35e7681ef
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-08-01 03:42:32 +0300

    remove console.log from 8-job

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -4,10 +4,8 @@ function createPushNotificationsJobs(jobs, queue) {
     jobs.forEach((jobData) => {
         const job = queue.create('push_notification_code_3', jobData);
         job.save((err) => {
-            console.log(job.id);
             if (!err) console.log(`Notification job created: ${job.id}`);
         });
-        console.log(job)
         job.on('complete', () => console.log(`Notification job ${job.id} completed`));
         job.on('failed', (errorMessage) => console.log(`Notification job ${job.id} failed: ${errorMessage}`));
         job.on('progress', (progress) => console.log(`Notification job ${job.id} ${progress}% complete`));
